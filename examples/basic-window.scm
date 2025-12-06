0c3d839a325b8b6032edac2cc4540eabe7c43359
Caelorum Spectatrix <clrum specttrx>
2025-12-06 20:42:16 +0100

    Add "with-..." wrappers

--- examples/basic-window.scm
+++ examples/basic-window.scm
@@ -4,14 +4,15 @@
 (init-window 800 450 "raylib [core] example - basic window")
 
 (let loop ()
-  (begin-drawing)
-  (clear-background RAYWHITE)
-  (draw-text "Congrats! You created your first window!"
-             190
-             200
-             20
-             LIGHTGRAY)
-  (end-drawing)
-  (if (window-should-close?) #f (loop)))
+  (with-drawing
+   (lambda ()
+     (clear-background RAYWHITE)
+     (draw-text "Congrats! You created your first window!"
+                190
+                200
+                20
+                LIGHTGRAY)))
+  (unless (window-should-close?)
+    (loop)))
 
 (close-window)
